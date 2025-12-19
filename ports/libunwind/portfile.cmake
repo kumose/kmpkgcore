a15a02ef720ae035313dbedaa04c5c8c3ca4dbd1
kmdo <kmdo@kumose.cc>
2025-12-19 17:37:46 +0800

    # import summary start ##### operator: Kmdo when: 2025-12-19T17:37:46+08:00 original.hash: a20d95d3c76906363896754eb1cc93db2a694f16 original.author: toge original.date: 2025-12-18 06:23:11 +0900 original.message: [libjpeg-turbo] update to 3.1.3 (#48813)
    original.remote: https://github.com/microsoft/vcpkg.git
    ports.count: 1
    ports.cost.us: 9223372036854775
    # import summary end #####

---
+++ ports/libunwind/portfile.cmake
@@ -0,0 +1,25 @@
+kmpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO "libunwind/libunwind"
+    REF "v${VERSION}"
+    HEAD_REF master
+    SHA512 0619716b2385375d618d84b1e9a75c42a7fa86d452c7c3168b4aa78c6bda629c8bb5e3a984a642277e9949c1b7dc39d5e21ae9d2670437182c7b797a14544cfa
+    PATCHES
+        liblzma.diff
+)
+
+kmpkg_find_acquire_program(PKGCONFIG)
+
+kmpkg_configure_make(
+    SOURCE_PATH "${SOURCE_PATH}"
+    AUTOCONFIG
+    OPTIONS
+        --disable-documentation
+        --disable-tests
+        --disable-zlibdebuginfo
+        --enable-minidebuginfo
+)
+kmpkg_install_make()
+kmpkg_fixup_pkgconfig()
+
+kmpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
