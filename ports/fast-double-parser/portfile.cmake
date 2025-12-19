0f13a56c028fe43b5aa1a9e5526abee421889c9a
kmdo <kmdo@kumose.cc>
2025-12-20 03:17:58 +0800

    # import summary start ##### operator: Kmdo when: 2025-12-20T03:17:58+08:00 original.hash: d2452281016c6d77b6ef199d599d9929eafe4807 original.author: toge original.date: 2025-12-19 04:49:30 +0900 original.message: [idyntree] update to 14.2.0 (#48945)
    original.remote: https://github.com/microsoft/vcpkg.git
    ports.count: 1
    ports.cost.us: 9223372036854775
    # import summary end #####

---
+++ ports/fast-double-parser/portfile.cmake
@@ -0,0 +1,21 @@
+set(KMPKG_BUILD_TYPE release) # header-only
+
+kmpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO lemire/fast_double_parser
+    REF "v${VERSION}"
+    SHA512 143f5d920159c5fc6d516417d14f297f7ba79764bab794ed6337dff73add7adcf99f27c078cd0e83a2907c5ec1143a247d85fc229eedcaf74d7710bab0adbd76
+    HEAD_REF master
+)
+
+kmpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DBUILD_TESTING=OFF
+)
+
+kmpkg_cmake_install()
+
+kmpkg_cmake_config_fixup(PACKAGE_NAME fast_double_parser)
+
+kmpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
