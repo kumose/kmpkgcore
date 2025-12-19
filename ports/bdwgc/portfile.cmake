6358f0bdd3f26fcbdef9c3ba282ce262ed0f3e20
kmdo <kmdo@kumose.cc>
2025-12-20 03:20:48 +0800

    # import summary start ##### operator: Kmdo when: 2025-12-20T03:20:48+08:00 original.hash: d2452281016c6d77b6ef199d599d9929eafe4807 original.author: toge original.date: 2025-12-19 04:49:30 +0900 original.message: [idyntree] update to 14.2.0 (#48945)
    original.remote: https://github.com/microsoft/vcpkg.git
    ports.count: 2
    ports.cost.us: 9223372036854775
    # import summary end #####

---
+++ ports/bdwgc/portfile.cmake
@@ -0,0 +1,26 @@
+kmpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO bdwgc/bdwgc
+    REF "v${VERSION}"
+    SHA512 af8dddd97390e2c44ef5d5bb47f5e4dac43b1932927fbe2154525f88cae40424af26f20c0cf282960383454d7af1a4139fad85bfc208f10191ef5828786fbae3
+    HEAD_REF master
+)
+
+kmpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -Denable_cplusplus=ON
+        -Denable_docs=OFF
+        -DCFLAGS_EXTRA=-I${CURRENT_INSTALLED_DIR}/include # for libatomic_ops
+)
+
+kmpkg_cmake_install()
+kmpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/bdwgc)
+kmpkg_copy_pdbs()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+kmpkg_fixup_pkgconfig()
+
+file(INSTALL "${SOURCE_PATH}/README.QUICK" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
