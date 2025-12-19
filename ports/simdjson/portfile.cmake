5294e3535413bc78863512fb759094b74063bc26
kmdo <kmdo@kumose.cc>
2025-12-20 01:47:30 +0800

    # import summary start ##### operator: Kmdo when: 2025-12-20T01:47:30+08:00 original.hash: d2452281016c6d77b6ef199d599d9929eafe4807 original.author: toge original.date: 2025-12-19 04:49:30 +0900 original.message: [idyntree] update to 14.2.0 (#48945)
    original.remote: https://github.com/microsoft/vcpkg.git
    ports.count: 1
    ports.cost.us: 9223372036854775
    # import summary end #####

---
+++ ports/simdjson/portfile.cmake
@@ -0,0 +1,44 @@
+kmpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO simdjson/simdjson
+    REF "v${VERSION}"
+    HEAD_REF master
+    SHA512 6d1588227dce4ad51aa0efcb2a07fd582181973bb2022480f5e9ab6393885d28d328b3c24f79f0fbf08ac59072899ddf57c2ff6dc4643665d9741be73a9b8b2e
+)
+
+kmpkg_check_features(
+    OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        exceptions SIMDJSON_EXCEPTIONS
+        threads    SIMDJSON_ENABLE_THREADS
+    INVERTED_FEATURES
+        deprecated SIMDJSON_DISABLE_DEPRECATED_API
+        utf8-validation SIMDJSON_SKIPUTF8VALIDATION
+)
+
+string(COMPARE EQUAL "${KMPKG_LIBRARY_LINKAGE}" "static" SIMDJSON_BUILD_STATIC)
+
+kmpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DSIMDJSON_JUST_LIBRARY=ON
+        -DSIMDJSON_SANITIZE_UNDEFINED=OFF
+        -DSIMDJSON_SANITIZE=OFF
+        -DSIMDJSON_SANITIZE_THREADS=OFF
+        -DSIMDJSON_BUILD_STATIC=${SIMDJSON_BUILD_STATIC}
+        -DSIMDJSON_DEVELOPMENT_CHECKS=OFF
+        -DSIMDJSON_VERBOSE_LOGGING=OFF
+        ${FEATURE_OPTIONS}
+)
+
+kmpkg_cmake_install()
+
+kmpkg_copy_pdbs()
+
+kmpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/${PORT}")
+
+kmpkg_fixup_pkgconfig()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" "${CURRENT_PACKAGES_DIR}/debug/share")
+
+kmpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE" "${SOURCE_PATH}/LICENSE-MIT")
