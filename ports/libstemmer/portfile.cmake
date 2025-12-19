04efe25016da042dff866c56a8a9c09419ef9c6c
kmdo <kmdo@kumose.cc>
2025-12-20 01:40:49 +0800

    # import summary start ##### operator: Kmdo when: 2025-12-20T01:40:49+08:00 original.hash: d2452281016c6d77b6ef199d599d9929eafe4807 original.author: toge original.date: 2025-12-19 04:49:30 +0900 original.message: [idyntree] update to 14.2.0 (#48945)
    original.remote: https://github.com/microsoft/vcpkg.git
    ports.count: 2
    ports.cost.us: 9223372036854775
    # import summary end #####

---
+++ ports/libstemmer/portfile.cmake
@@ -0,0 +1,24 @@
+kmpkg_check_linkage(ONLY_STATIC_LIBRARY)
+string(SUBSTRING "${VERSION}" 5 -1 VERSION)
+
+kmpkg_download_distfile(ARCHIVE
+    URLS "https://snowballstem.org/dist/libstemmer_c-${VERSION}.tar.gz"
+    FILENAME "libstemmer_c-${VERSION}.tar.gz"
+    SHA512 a61a06a046a6a5e9ada12310653c71afb27b5833fa9e21992ba4bdf615255de991352186a8736d0156ed754248a0ffb7b7712e8d5ea16f75174d1c8ddd37ba4a
+)
+
+kmpkg_extract_source_archive(
+    SOURCE_PATH
+    ARCHIVE "${ARCHIVE}"
+)
+
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
+
+kmpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS_DEBUG -DDISABLE_INSTALL_HEADERS=ON
+)
+
+kmpkg_cmake_install()
+
+kmpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING")
