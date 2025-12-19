36cc828c606756b50b4bf23aa979bf6c1b2a757f
kmdo <kmdo@kumose.cc>
2025-12-19 16:10:06 +0800

    # import summary start ##### operator: Kmdo when: 2025-12-19T16:10:06+08:00 original.hash: a20d95d3c76906363896754eb1cc93db2a694f16 original.author: toge original.date: 2025-12-18 06:23:11 +0900 original.message: [libjpeg-turbo] update to 3.1.3 (#48813)
    original.remote: https://github.com/microsoft/vcpkg.git
    ports.count: 1
    ports.cost.us: 9223372036854775
    # import summary end #####

---
+++ ports/jemalloc/portfile.cmake
@@ -0,0 +1,58 @@
+kmpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO jemalloc/jemalloc
+    REF 54eaed1d8b56b1aa528be3bdd1877e59c56fa90c
+    SHA512 527bfbf5db9a5c2b7b04df4785b6ae9d445cff8cb17298bf3e550c88890d2bd7953642d8efaa417580610508279b527d3a3b9e227d17394fd2013c88cb7ae75a
+    HEAD_REF master
+    PATCHES
+        fix-configure-ac.patch
+        preprocessor.patch
+        fix-host-use-libc.patch # https://github.com/jemalloc/jemalloc/commit/45249cf5a9cfa13c2c62e68e272a391721523b4b
+)
+if(KMPKG_TARGET_IS_WINDOWS)
+    set(opts "ac_cv_search_log=none required" "--without-private-namespace")
+endif()
+
+kmpkg_make_configure(
+    AUTORECONF
+    SOURCE_PATH "${SOURCE_PATH}"
+    DISABLE_MSVC_WRAPPERS
+    DISABLE_MSVC_TRANSFORMATIONS
+    OPTIONS ${opts}
+)
+
+kmpkg_make_install()
+
+if(KMPKG_TARGET_IS_WINDOWS)
+    file(COPY "${SOURCE_PATH}/include/msvc_compat/strings.h" DESTINATION "${CURRENT_PACKAGES_DIR}/include/jemalloc/msvc_compat")
+    kmpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/jemalloc/jemalloc.h" "<strings.h>" "\"msvc_compat/strings.h\"")
+    if(KMPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
+        file(COPY "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/lib/jemalloc.lib" DESTINATION "${CURRENT_PACKAGES_DIR}/lib")
+        file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/bin")
+        file(RENAME "${CURRENT_PACKAGES_DIR}/lib/jemalloc.dll" "${CURRENT_PACKAGES_DIR}/bin/jemalloc.dll")
+    endif()
+    if(NOT KMPKG_BUILD_TYPE)
+        if(KMPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
+            file(COPY "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/lib/jemalloc.lib" DESTINATION "${CURRENT_PACKAGES_DIR}/debug/lib")
+            file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/debug/bin")
+            file(RENAME "${CURRENT_PACKAGES_DIR}/debug/lib/jemalloc.dll" "${CURRENT_PACKAGES_DIR}/debug/bin/jemalloc.dll")
+        endif()
+    endif()
+    if(KMPKG_LIBRARY_LINKAGE STREQUAL "static")
+        kmpkg_replace_string("${CURRENT_PACKAGES_DIR}/lib/pkgconfig/jemalloc.pc" "install_suffix=" "install_suffix=_s")
+        if(NOT KMPKG_BUILD_TYPE)
+            kmpkg_replace_string("${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig/jemalloc.pc" "install_suffix=" "install_suffix=_s")
+        endif()
+    endif()
+endif()
+
+kmpkg_fixup_pkgconfig()
+
+kmpkg_copy_pdbs()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/tools")
+
+# Handle copyright
+file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
